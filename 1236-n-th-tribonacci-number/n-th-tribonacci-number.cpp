e8e5ca6cbbde712bde5ddef6ae1e92832a746560
Swati9067 <101699624+Swati9067@users.noreply.github.com>
2025-11-18 02:28:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (21.84%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+
+        if(n<=1) return n;
+        if(n==1) return 1;
+        vector<int> store(n+1,0);
+        store[0] =0;
+        store[1] =1;
+        store[2] =1;
+
+        for(int i =3; i<store.size(); i++){
+            store[i] = store[i-1] +store[i-2]+store[i-3];
+        }
+
+        return store[store.size()-1];
+
+    }
+};
