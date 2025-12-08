16e0979506782337648e470ad776839b84e41692
Swati Suman <101699624+Swati9067@users.noreply.github.com>
2025-12-09 01:35:50 +0530

    Time: 10 ms (92.95%) | Memory: 45.9 MB (35.60%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& p, int l) {
+    
+        sort(p.begin(), p.end());
+        int i =0, j = p.size()-1;
+        int sum = 0;
+        while(i<=j)
+        {
+             if(p[i]+p[j] <=l) i++;
+             j--;
+             sum ++;
+             
+
+        }
+
+        return sum;
+    }
+};
