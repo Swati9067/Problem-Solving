3ee5138599f2c0b23a231ef360fa7e224721dbd7
Swati Suman <101699624+Swati9067@users.noreply.github.com>
2025-12-17 01:47:53 +0530

    Time: 56 ms (29.25%) | Memory: 29 MB (89.53%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,113 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-      vector<vector<int>> ans;
-      set<vector<int>> st;
-      int i,k,j;
-      i=j=k=0;
-      sort(nums.begin(),nums.end());
-      while(i<nums.size()-2){
-          j = i+1;
-          k = nums.size()-1;
-          while(j<k){
-            int sum = nums[i]+nums[j]+nums[k];
-            if(sum > 0) {
-                int x = nums[k];
-                while(k>0 && x==nums[k])
-                k--;
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
             }
-            else if(sum < 0){ 
-                int x = nums[j];
-                while(j<nums.size() && x==nums[j])
-                j++;
-            }
-            else{
-                st.insert({nums[i],nums[j],nums[k]});
-                int x =nums[j];
-                 while(j<nums.size() && x==nums[j])
-                 j++;
-                 x = nums[k];
-                 while(k>0 && x==nums[k])
-                 k--;
-            }
-          }
-          i++;
-      }
-      for(auto it:st){
-        ans.push_back(it);
-      }
-      return ans;
+            
+            int j = i + 1;
+            int k = nums.size() - 1;
 
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
 
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.push_back({nums[i], nums[j], nums[k]});
+                    j++;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       /* vector<vector<int>> ans;
-        set<vector<int>> st;
-        sort(nums.begin(),nums.end());
-        for(int i =0; i<nums.size()-2; i++)
-        {
-            int j = i+1;
-            int k = nums.size()-1;
-            long sum =0;
-            while(j<k){
-                
-                sum= nums[i]+nums[j]+nums[k];
-                if(sum == 0)
-                {   vector<int> temp;
-                    temp.push_back(nums[i]);
-                    temp.push_back(nums[j]);
-                    temp.push_back(nums[k]);
-                    st.insert(temp);
-                 j++;
-                 k--;
-                }
-
-                else if (sum <0) k++;
-                else j++;
-            }
-        }
-         for( auto x: st){
-            ans.push_back(x);
-        }
-        return ans;*/
-
-
-        /*vector<vector<int>> ans;
-        set <vector<int>> st;
-        for(int i =0; i<nums.size()-2; i++){
-            for(int j =i+1; j<nums.size()-1; j++){
-                for(int k =j+1;k<nums.size(); k++ ){
-
-                    if(nums[i]+nums[j]+nums[k]==0){
-                        vector<int> temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[j]);
-                        temp.push_back(nums[k]);
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
                     }
                 }
             }
         }
-        for( auto x: st){
-            ans.push_back(x);
-        }
-        return ans;*/
+        return res;        
     }
 };
