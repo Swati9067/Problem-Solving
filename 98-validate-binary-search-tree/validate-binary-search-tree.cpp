55cdc65d15b03ec3641ebeffe9cb84d77309ed4c
Swati Suman <101699624+Swati9067@users.noreply.github.com>
2026-01-05 22:23:15 +0530

    Time: 1 ms (7.65%) | Memory: 22.2 MB (8.47%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void traversal ( vector<int> &storeInOrder,TreeNode* root){
+        if(!root) return;
+        traversal (storeInOrder, root->left);
+        storeInOrder.push_back(root->val);
+        traversal (storeInOrder, root->right);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        vector<int> storeInOrder;
+        traversal (storeInOrder, root);
+        //vector<int> sorted = sort(storeInOrder.begin(), storeInOrder.end()); sort function return void
+        vector<int> sorted = storeInOrder;
+        //sort(storeInOrder.begin(), storeInOrder.end());
+      
+        for(int i =0; i<sorted.size()-1; i++){
+           if(sorted [i]>=sorted[i+1]) return false;
+        }
+        return true;;
+    }
+};
